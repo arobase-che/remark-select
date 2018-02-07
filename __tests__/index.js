7134597fb3b28ae872361b9527ba6fe42149c3e4
ache <ache@ache.one>
2018-02-07 03:47:26 +0100

    Write some tests

--- __tests__/index.js
+++ __tests__/index.js
@@ -25,7 +25,7 @@ const renderRaw = text => unified()
   .use(stringify)
   .processSync(text);
 
-test('select', t => {
+test('select-snap', t => {
   const {contents} = render(file(join(__dirname, 'select.md')));
   t.snapshot(contents);
 });
@@ -44,22 +44,64 @@ test('select-simple', t => {
 </select></p>`);
 });
 
-test('select-raw-normal', t => {
+test('select-raw-normal-snap', t => {
   const {contents} = renderRaw(file(join(__dirname, 'select.md')));
   t.snapshot(contents);
 });
 
-test('select-raw', t => {
+test('select-raw-snap', t => {
   const {contents} = renderRaw(file(join(__dirname, 'select-raw.md')));
   t.snapshot(contents);
 });
 
-test.todo('id text');
-test.todo('class');
-test.todo('classes');
-test.todo('key-value');
+test('select-id', t => {
+  const {contents} = render('[ Start | Restart | Quit |v]{#hedgedog}');
+  t.is(contents, `<p><select id="hedgedog">\
+<option value="Start">Start</option>\
+<option value="Restart">Restart</option>\
+<option value="Quit">Quit</option>\
+</select></p>`);
+});
+
+test('select-multi-id', t => {
+  const {contents} = render('[ Luigi | Mario | Peach | Falco | Zelda |v]{#ssb #nintendo #link #starFox}');
+  t.is(contents, `<p><select id="ssb">\
+<option value="Luigi">Luigi</option>\
+<option value="Mario">Mario</option>\
+<option value="Peach">Peach</option>\
+<option value="Falco">Falco</option>\
+<option value="Zelda">Zelda</option>\
+</select></p>`);
+});
+
+test('select-class', t => {
+  const {contents} = render('[ Luigi | Mario | Peach |v]{.nintendo}');
+  t.is(contents, `<p><select class="nintendo">\
+<option value="Luigi">Luigi</option>\
+<option value="Mario">Mario</option>\
+<option value="Peach">Peach</option>\
+</select></p>`);
+});
+
+test('select-classes', t => {
+  const {contents} = render('[ Luigi | Mario | Peach | Falco | Zelda |v]{.mario .link .starFox}');
+  t.is(contents, `<p><select class="mario link starFox">\
+<option value="Luigi">Luigi</option>\
+<option value="Mario">Mario</option>\
+<option value="Peach">Peach</option>\
+<option value="Falco">Falco</option>\
+<option value="Zelda">Zelda</option>\
+</select></p>`);
+});
+
+test('select-key-value', t => {
+  const {contents} = render('[ Falco | Fox |v]{game="Star Fox"}');
+  t.is(contents, `<p><select game="Star Fox">\
+<option value="Falco">Falco</option>\
+<option value="Fox">Fox</option>\
+</select></p>`);
+});
+
 test.todo('classes key-value id');
 test.todo('overwrite class');
 test.todo('overwrite id');
-test.todo('multiple id');
-
