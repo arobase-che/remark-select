33686255bae7688daccd59615d8d69de33b9d8ef
ache <ache@ache.one>
2018-05-23 11:52:48 +0200

    Add more comments

--- app.js
+++ app.js
@@ -5,13 +5,19 @@ const END_RGX = /(\| *[vV] *])({[^\n]*})?/;
 
 const parseAttr = require('md-attr-parser');
 
+/* The function that locate a text input
+ * Used by remark
+ */
 function locator(value, fromIndex) {
   const index = value.indexOf(START, fromIndex);
   return index;
 }
 
+/* The main plugin function */
 function plugin() {
-  let END = '|v]';
+  let END = '|v]'; // The default value isn't important
+
+  // Function that check the syntax and return a text input node
   function inlineTokenizer(eat, value) {
     if (!this.options.gfm || !value.startsWith(START)) {
       return;
@@ -19,11 +25,14 @@ function plugin() {
 
     let subvalue = '';
     let index = 1;
-    const length = value.length;
+    const {length} = value;
+
+    /* Not sure about that ... */
     const now = eat.now();
     now.column += 2;
     now.offset += 2;
 
+    // Extract the options
     let ret = null;
     if ((ret = value.substr(1).match(END_RGX)) && index < length) {
       subvalue += value.substr(1, ret.index);
@@ -34,8 +43,9 @@ function plugin() {
       return;
     }
 
+    // Extract the attributes
     let letsEat = '';
-    let prop = {class: undefined /* [] */, id: undefined};
+    let prop = {/* class: undefined  [] , id: undefined */};
     if (value.charAt(index) === '{') {
       const res = parseAttr(value, index);
       letsEat = res.eaten;
@@ -43,6 +53,7 @@ function plugin() {
       prop = res.prop;
     }
 
+    // Return the select node
     if (index <= length) {
       return eat(START + subvalue + END + letsEat)({
         type: 'select',
